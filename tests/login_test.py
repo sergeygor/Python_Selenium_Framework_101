8aff7bc5fecd9192dce5b4e05f3cd34c9d15eb7c
sergeygor <sgordeev68git@gmail.com>
2019-10-05 12:23:10 -0700

    1. Added pytest functions to login_test 2. Created class TestLogin():

--- tests/login_test.py
+++ tests/login_test.py
@@ -2,27 +2,28 @@ from selenium import webdriver
 import time
 import pytest
 
-@pytest.fixture(scope="session")
-def test_setup():
-    global driver
-    driver = webdriver.Chrome('/Users/sergeygordeev/Desktop/EDU/Python/Python_Selenium_Framework_101/drivers/chromedriver')
-    driver.implicitly_wait(10)
-    driver.maximize_window()
-    yield
-    time.sleep(2)
-    driver.close()
-    driver.quit()
 
+class TestLogin:
+    @pytest.fixture(scope="class")
+    def test_setup(self):
+        global driver
+        driver = webdriver.Chrome('/Users/sergeygordeev/Desktop/EDU/Python/Python_Selenium_Framework_101/drivers/chromedriver')
+        driver.implicitly_wait(10)
+        driver.maximize_window()
+        yield
+        time.sleep(2)
+        driver.close()
+        driver.quit()
 
-def test_login(test_setup):
-    driver.get('https://opensource-demo.orangehrmlive.com/index.php/auth/login')
-    driver.find_element_by_id('txtUsername').send_keys('Admin')
-    driver.find_element_by_id('txtPassword').send_keys('admin123')
-    driver.find_element_by_id('btnLogin').click()
+    def test_login(self, test_setup):
+        driver.get('https://opensource-demo.orangehrmlive.com/index.php/auth/login')
+        driver.find_element_by_id('txtUsername').send_keys('Admin')
+        driver.find_element_by_id('txtPassword').send_keys('admin123')
+        driver.find_element_by_id('btnLogin').click()
 
-def test_logout(test_setup):
-    driver.find_element_by_id('welcome').click()
-    driver.find_element_by_link_text('Logout').click()
+    def test_logout(self, test_setup):
+        driver.find_element_by_id('welcome').click()
+        driver.find_element_by_link_text('Logout').click()
 
 
 
